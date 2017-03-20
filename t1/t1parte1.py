961953687e286cbe323be0b57321b718fdb3b404
Mikael <mikaelsantellano@hotmail.com>
2017-03-19 22:22:48 -0300

    Add files via upload

---
+++ t1/t1parte1.py
@@ -0,0 +1,73 @@
+# Exercício 1
+
+def soma(x +y):
+    return (pow(x^2)+(pow(y^2))
+
+#Exercicio 2
+
+def hasEqHeads(l1,l2):
+    return l1[0] == l2 [0]
+
+#exercicio 3
+            
+def Srst(x): return "Sr " + x
+
+def lista(l1):
+    return list(map(Srst, l1))
+
+# exercicio 4
+
+def contEspaco (c):
+    return c in ' '
+
+def espaco (x):
+    return len(list(filter(contEsp,x)))
+
+#Exercicio 5 
+
+def listcalc (n):
+    return ((3n*2) + (2/n +1))
+
+def listaa (x):
+    return list(map(listc,x))        
+            
+#exercicio 6
+            
+def lisNeg(x):
+    return x < 0        
+
+def lisN(l1):
+    return list(filter(lisNeg,l1))
+            
+#exercicio 7
+
+def auxListaCem (x):
+    return ((x >= 1) & (x <= 100))        
+
+def listaCem(lista):
+    return list(filter(auxListaCem, lista))
+
+#exercicio 8
+
+def paresAux(x):
+    return x % 2 == 0
+            
+def funcPares(listaa):
+    return list(filter(paresAux, listaa))
+
+#exercicio 9
+
+def charFound(c,s):
+    return s.count (c) >= 1       
+            
+# exercicio 10
+
+def listaHtml(x):
+    return '<b>'+ x + '</b>'         
+
+def listaa (l1):
+    return list(map(listaHtml,l1)        
+
+            
+
+
---
+++ t1/t1parte2.py
@@ -0,0 +1,22 @@
+#1
+
+def stringSr (l1):
+    return list(map(lambda l1: 'Sr ' + l1, l1))
+#2
+
+def calcl(l2):
+    return list(map(lambda l2: ((3*l2*2) + 2/l2+1, lista)))
+#3
+
+def FunNome(l3):
+    return list(filter(lambda l3: l3[-1] == 'a', lista))
+
+#4
+
+def idades (lista):
+    return list(filter(lambda lista: 2017 - lista >= 1970, lista))
+
+#5
+
+def numeroProgFunc (numeros=[1,2,3,4,]):
+    return list(map(lambda numeros: pow(numeros,2),numeros))
