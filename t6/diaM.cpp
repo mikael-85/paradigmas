8bfdb97b04e94e972333d05d7cfbec9f186e07e5
Mikael <mikaelsantellano@hotmail.com>
2017-05-22 00:05:00 -0300

    t6
    Atualizando...

--- t6/diaM.cpp
+++ t6/diaM.cpp
@@ -7,11 +7,6 @@
 
 // classe pai biscoito
 
-/*pequeno: 2 a 4 cm - 1 real
-  médio:  5 a 7 cm - 2 reais
-  grande 8 a  13  - 3 reais
-*/
-
 class Biscoito{
 private:
     int taman;
@@ -19,22 +14,19 @@ private:
 public:
     Biscoito(){
         taman=preco=0;
-        }
-    setPreco(int TP){
-        taman = TP;
-// escolher preço pelo tamanho
-    switch (taman){
-        case 1:
-            return preco = 1;
-            break;
-        case 2:
-            return preco = 2;
-            break;
-        case 3:
-            return preco = 3;
-            break;
-        }
-
+    }
+	setAll(float area){
+		if(area>=2 && area<=4){
+			taman = 1;
+			preco = 1;
+		}else if(area>=5 && area<=7){
+			preco = 2;
+			taman = 2;
+		}else if(area>= 8 && area<=13){
+			preco = 3;
+			taman = 3;
+		}
+	}
     };
 
 
@@ -46,30 +38,23 @@ private:
 public:
     Circle(float r){
         raio = r;
-        ar = 0.0;
-        }
- float area(){
         ar = pi*(raio*raio);
-
-        }
-
-
+        setAll(ar);
+    }
 };
 
 class Retangulo : public Biscoito{
 private:
     float lad;
     float altura;
-    float area;
+    float ar;
 public:
     Retangulo (float a, float b){
         lad = a;
         altura =b;
-        }
-
-    areaQ(int a){
-        a = lad * altura;
-        }
+        ar = lad * altura;
+        setAll(ar);
+    }
 };
 
 class Triangulo : public Biscoito{
@@ -81,9 +66,8 @@ public:
     Triangulo (float b, float c){
         base =b;
         altura =c;
-    }
-    int area(){
-        ar = (base*altura)/2
+        ar = (base*altura)/2;
+        setAll(ar);
     }
 };
 
@@ -91,27 +75,25 @@ public:
 // fim das classes filho
 
 int main(){
-    int num;
+	vector<Circulo> circulos;
+    vector<Triangulo> triangulos;
+	vector<Retangulo> retangulos;
+    int num,i;
     count<< "quantos biscoitos deseja produzir? ";
     cin >> num;
-
-    vector<Circulo> circulos;
-    vector<Triangulo> triangulos;
-    vector<Retangulo> retangulos;
-
 
     for(int i =0; i < num; i++){
         tipo = rand()%3;
 
         switch (tipo){
             case 0:
-                cir.push_back(Circulo( (rand()%) / ));
+                circulos.push_back(Circulo( (rand()%) / ));
                 break;
             case 1:
-                cir.push_back(Retangulo( (rand()%) / ));
+                triangulos.push_back(Retangulo( (rand()%) / ));
                 break;
             case 2:
-                cir.push_back(Triangulo( (rand()%) / ));
+                retangulos.push_back(Triangulo( (rand()%) / ));
                 break;
         }
 
